343907b7bb5d9ebc134184871e2e0f4e78fcc237
peterkrejzl <pkrejzl@seznam.cz>
2016-08-13 13:30:56 +0200

    upd

---
+++ VariousNeuralNetworks/RNN.py
@@ -0,0 +1,22 @@
+'''
+based on http://www.wildml.com/2015/09/recurrent-neural-networks-tutorial-part-1-introduction-to-rnns/
+
+'''
+
+import csv
+import itertools
+import operator
+import numpy as np
+import nltk
+import sys
+from datetime import datetime
+from utils import *
+
+import matplotlib.pyplot as plt
+#%matplotlib inline
+
+
+
+
+
+print('done')
